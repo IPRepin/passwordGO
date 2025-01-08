33954d64f3cd6ecf6b679ee41f5a22804614984d
Pavel Repin <76727704+IPRepin@users.noreply.github.com>
2025-01-08 13:40:20 +0300

    go tidy

--- main.go
+++ main.go
@@ -34,7 +34,7 @@ func main() {
 	if err != nil {
 		output.PrintError("Не удалось найти env файл")
 	}
-	vault := account.NewVault(files.NewJsonDb("data.vault"), *encrypter.NewEncrypter())
+	vault := account.NewVault(files.NewJsonDb("data.json"), *encrypter.NewEncrypter())
 	// vault := account.NewVault(cloud.NewCloudDb("https://a.ru"))
 Menu:
 	for {
